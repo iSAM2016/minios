585694816414117d94392b66877325503d4b3777
isam2016 <isam2016@qq.com>
2022-01-17 21:55:21 +0800

    add makefile

---
+++ lib/kernel/bitmap.h
@@ -0,0 +1,20 @@
+// //  位图的结构
+// #ifndef __LIB_KERNEL_BITMAP_H
+// #define __LIB_KERNEL_BITMAP_Ha
+// #include "global.h"
+// #define BITMAP_MASK 1 // 常量
+// // 位图的结构
+// struct bitmap
+// {
+//     uint32_t btmp_tytes_len; // 位图的字节长度
+//     // 在遍历位图的时候，整体上以字节为单位，细节上是以位为单位，
+//     // 所以此处位图的只恨必须是单字节
+//     uint8_t *bits;
+// };
+// void bitmap_init(struct bitmap *bitmap);
+// bool bitmap_scan_test(struct bitmap *bimp, ui){
+//     /* data */
+// };
+// )
+
+// #elif
