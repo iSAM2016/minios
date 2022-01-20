6c0422e3d2509de94281f6d7f3f677a1727f0965
isam2016 <isam2016@qq.com>
2022-01-20 18:22:12 +0800

    bitmap

--- lib/kernel/bitmap.h
+++ lib/kernel/bitmap.h
@@ -1,20 +1,15 @@
-// //  位图的结构
-// #ifndef __LIB_KERNEL_BITMAP_H
-// #define __LIB_KERNEL_BITMAP_Ha
-// #include "global.h"
-// #define BITMAP_MASK 1 // 常量
-// // 位图的结构
-// struct bitmap
-// {
-//     uint32_t btmp_tytes_len; // 位图的字节长度
-//     // 在遍历位图的时候，整体上以字节为单位，细节上是以位为单位，
-//     // 所以此处位图的只恨必须是单字节
-//     uint8_t *bits;
-// };
-// void bitmap_init(struct bitmap *bitmap);
-// bool bitmap_scan_test(struct bitmap *bimp, ui){
-//     /* data */
-// };
-// )
-
-// #elif
+//  位图的结构
+#ifndef __LIB_KERNEL_BITMAP_H
+#define __LIB_KERNEL_BITMAP_Ha
+#include "global.h"
+#define BITMAP_MASK 1 // 常量 f用来在位图中逐位判断。 通过按位与&来判断是否为1
+// 位图的结构
+struct bitmap
+{
+    uint32_t btmp_tytes_len; // 位图的字节长度-在遍历位图的时候，整体上以字节为单位，细节上是以位为单位， unsigned int 4个字节 32位
+    uint8_t *bits;           // 位图指针 unsigned char 1个字节8位
+};
+void bitmap_init(struct bitmap *bitmap);
+bool bitmap_scan_test(struct bitmap *bimp, uint32_t bit_idx);
+int bitmap_scan(struct bitmap *btmp, uint32_t cnt);
+#endif
