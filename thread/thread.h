55b22474408af7f63e5f755a65439601642ccaf9
isam2016 <isam2016@qq.com>
2022-02-10 18:42:48 +0800

    修改

--- thread/thread.h
+++ thread/thread.h
@@ -2,10 +2,10 @@
 #define __THREAD_THREAD_H
 #include "stdint.h"
 
-/* 自定义通用函数类型，他将在很多线程函数中作为形参类型*/
+/* 自定义通用函数类型,它将在很多线程函数中做为形参类型 */
 typedef void thread_func(void *);
 
-/* 进程或线程的状态*/
+/* 进程或线程的状态 */
 enum task_status
 {
     TASK_RUNNING,
@@ -15,20 +15,20 @@ enum task_status
     TASK_HANGING,
     TASK_DIED
 };
-/************** 中断栈 intr_stack *********************
- * 此结构用于中断发生是保护程序（线程或进程）的上下文环境：
- * 进程或线程被外部中断或软终端打断时， 会按照此结构压入上下文
- * 寄存器， intr_exit 中的出栈操作是次结构的逆操作
- * 此栈 在线程自己的内核栈中位置固定，所以在页的最顶端
- *
- **************************************************/
+
+/***********   中断栈intr_stack   ***********
+ * 此结构用于中断发生时保护程序(线程或进程)的上下文环境:
+ * 进程或线程被外部中断或软中断打断时,会按照此结构压入上下文
+ * 寄存器,  intr_exit中的出栈操作是此结构的逆操作
+ * 此栈在线程自己的内核栈中位置固定,所在页的最顶端
+ ********************************************/
 struct intr_stack
 {
-    uint32_t vec_no; //  kernel.S 宏 VECTOR中 pus h % 1 压入的中断号
+    uint32_t vec_no; // kernel.S 宏VECTOR中push %1压入的中断号
     uint32_t edi;
     uint32_t esi;
     uint32_t ebp;
-    uint32_t esp_dummy; //  虽然 pushad 把 esp 也压入，但 esp是不断变化的，所以会被 popad 忽略
+    uint32_t esp_dummy; // 虽然pushad把esp也压入,但esp是不断变化的,所以会被popad忽略
     uint32_t ebx;
     uint32_t edx;
     uint32_t ecx;
@@ -37,46 +37,52 @@ struct intr_stack
     uint32_t fs;
     uint32_t es;
     uint32_t ds;
-    /*以下由 cpu 从低特权级进入高特权级时压入 */
-    uint32_t err_code;
-    // err_code 会被压入在 eip 之后
+
+    /* 以下由cpu从低特权级进入高特权级时压入 */
+    uint32_t err_code; // err_code会被压入在eip之后
     void (*eip)(void);
     uint32_t cs;
     uint32_t eflags;
     void *esp;
     uint32_t ss;
 };
-/****************** 线程栈 thread_stack *****************
- * 线程自己的栈， 用于存储线程中待执行的函数
- * 此结构在线程自己的内车栈中位置不固定
- * 仅用在switch_to时保存线程的环境
- * 实际位置取决于实际运行情况
- *******************************************************/
+
+/***********  线程栈thread_stack  ***********
+ * 线程自己的栈,用于存储线程中待执行的函数
+ * 此结构在线程自己的内核栈中位置不固定,
+ * 用在switch_to时保存线程环境。
+ * 实际位置取决于实际运行情况。
+ ******************************************/
 struct thread_stack
 {
-    // 保护寄存器的值  在switch_to函数中在恢复这4个值
     uint32_t ebp;
     uint32_t ebx;
     uint32_t edi;
     uint32_t esi;
-    /* 线程第一次执行时候， eip 指向待调用的函数kernel_thread 其他时候，
-    eip 是指向switch_to 的返回地址*/
+
+    /* 线程第一次执行时,eip指向待调用的函数kernel_thread
+    其它时候,eip是指向switch_to的返回地址*/
     void (*eip)(thread_func *func, void *func_arg);
-    /******* 以下仅供第一次被调度上cpu 时使用*********/
-    /* 参数unused_ret 只为占位置成熟为返回地址*/
+
+    /*****   以下仅供第一次被调度上cpu时使用   ****/
+
+    /* 参数unused_ret只为占位置充数为返回地址 */
     void(*unused_retaddr);
-    thread_func *function; // 由kernel_thread 所调用的函数名
-    void *func_arg;        // 由kernel_thread 所调用的函数所需的参数
+    thread_func *function; // 由Kernel_thread所调用的函数名
+    void *func_arg;        // 由Kernel_thread所调用的函数所需的参数
 };
 
-/*进程或线程的pcb 程序控制块*/
+/* 进程或线程的pcb,程序控制块 */
 struct task_struct
 {
-    uint32_t *self_kstack;   // 各个内核线程都用自己的内核栈
-    enum task_status status; // 用于记录线程状态，其类型便是前面定义的枚举结构enum task_status
-    uint8_t priority;        // 线程优先级，进程或线程都要有个优先级，此优先级咱们用来决定进程或线程的时间片，即被调度到处理器上的运行时间
-    char name[16];           //用于记录任务（线程或进程）的名字，长度是 16
-    uint32_t stack_magic;    //栈的边界标记，用于检测栈的溢出
+    uint32_t *self_kstack; // 各内核线程都用自己的内核栈
+    enum task_status status;
+    uint8_t priority; // 线程优先级
+    char name[16];
+    uint32_t stack_magic; // 用这串数字做栈的边界标记,用于检测栈的溢出
 };
 
+void thread_create(struct task_struct *pthread, thread_func function, void *func_arg);
+void init_thread(struct task_struct *pthread, char *name, int prio);
+struct task_struct *thread_start(char *name, int prio, thread_func function, void *func_arg);
 #endif
